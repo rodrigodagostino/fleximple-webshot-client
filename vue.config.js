892d9c8dcfdf39c75b922e7ebedbe97c6fe1d15b
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-08-19 00:58:24 -0300

    Set outputDir to generate the distribution files inside the serverpublic folder, set devServer to be used instead of environmental variables

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
 module.exports = {
 	chainWebpack: config => {
-		config
-			.plugin( 'html' )
-			.tap( args => {
-				args[ 0 ].title = 'Fleximple Webshot'
-				return args
-			} )
+		config.plugin( 'html' ).tap( args => {
+			args[ 0 ].title = 'Fleximple Webshot'
+			return args
+		} )
+	},
+	outputDir: '../public',
+	devServer: {
+		proxy: {
+			'^/api': {
+				target: 'http://localhost:3080',
+				changeOrigin: true,
+			},
+		},
 	},
 }
